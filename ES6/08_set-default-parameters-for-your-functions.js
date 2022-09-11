30f74b13dbfa1068a86b7d078d64691e25512e31
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-09-11 23:02:59 +0700

    feat: add lession set-default-parameters-for-your-functions

---
+++ ES6/08_set-default-parameters-for-your-functions.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+console.log(increment(5, 2));
+console.log(increment(5));
+// Only change code above this line
