836ea3ae7365b84835e41da2828d5f9ddbb55b75
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-10-11 23:45:58 +0700

    feat: add lession use-destructuring-assignment-to-assign-variables-from-nested-objects

---
+++ ES6/14_use-destructuring-assignment-to-assign-variables-from-nested-objects.js
@@ -0,0 +1,7 @@
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
