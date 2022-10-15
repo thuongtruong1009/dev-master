83d66cf9824b6a8e2d92da7dd3516c0ae5b39a26
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-10-15 23:28:57 +0700

    feat: add lession use-destructuring-assignment-to-pass-an-object-as-a-functions-parameters

---
+++ ES6/17_use-destructuring-assignment-to-pass-an-object-as-a-functions-parameters.js
@@ -0,0 +1,10 @@
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+const half = ({ max, min }) => (max + min) / 2.0;
