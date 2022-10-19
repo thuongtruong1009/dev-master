638cbacf97dd9209a23400517fcf6f9314226968
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-10-19 20:38:58 +0700

    feat: add lession write-concise-declarative-functions-with-es6

---
+++ ES6/20_write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+
+bicycle.setGear(3);
+console.log(bicycle.gear);
