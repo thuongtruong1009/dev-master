223d906bfb2b73c326492d5a451c506bd811b304
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-11-03 20:35:23 +0700

    feat: add lession complete-a-promise-with-resolve-and-reject

---
+++ ES6/30_complete-a-promise-with-resolve-and-reject.js
@@ -0,0 +1,10 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
