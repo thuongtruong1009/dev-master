1433e052e5c5a0ef29c5b6e465064c9b43446b29
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-11-06 21:37:28 +0700

    feat: add lession handle-a-rejected-promise-with-catch

---
+++ ES6/33_handle-a-rejected-promise-with-catch.js
@@ -0,0 +1,18 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest.then(result => {
+    console.log(result);
+});
+
+makeServerRequest.catch(error => {
+    console.log(error);
+});
