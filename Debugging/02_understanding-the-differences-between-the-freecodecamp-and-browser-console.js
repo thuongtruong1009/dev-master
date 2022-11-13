9b7342e1331239b7c6cf8724a663379524c0c168
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-11-13 21:01:32 +0700

    feat: add lession understanding-the-differences-between-the-freecodecamp-and-browser-console

---
+++ Debugging/02_understanding-the-differences-between-the-freecodecamp-and-browser-console.js
@@ -0,0 +1,8 @@
+// Open your browser console.
+let output = "Get this to log once in the freeCodeCamp console and twice in the browser console";
+
+// Use console.log() to print the output variable.
+console.log(output);
+
+// Now, add console.clear() before your console.log() to clear the browser console, and pass the tests.
+console.clear();
