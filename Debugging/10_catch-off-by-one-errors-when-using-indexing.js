c3faa9b24f8d7dd637e518a9cd3f29effb469baf
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-12-19 23:30:35 +0700

    feat: add lession catch-off-by-one-errors-when-using-indexing

---
+++ Debugging/10_catch-off-by-one-errors-when-using-indexing.js
@@ -0,0 +1,11 @@
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+
+    for (let i = 0; i < len; i++) {
+
+        console.log(firstFive[i]);
+    }
+}
+
+countToFive();
